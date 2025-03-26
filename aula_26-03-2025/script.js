c345279f6f951c2f07b154cb353976a35f95ca4f
LucasFK035 <lucaskunrath9@gmail.com>
2025-03-26 16:40:03 -0300

    Update script.js

---
+++ aula_26-03-2025/script.js
@@ -0,0 +1,18 @@
+console.log("PREÇO DO PEDIDO:")
+
+/* Tamanho */
+let pequeno = 10;
+let medio = 15;
+let grande = 20;
+
+/* Sabor */
+let queijo = 3.5;
+let calabresa = 4;
+let morango = 8;
+
+/* Entrega e busca do pedido */
+let entregar = 5;
+let buscar = 0;
+
+/* Tamanho */
+let calcular = quantidade*tamanho;
